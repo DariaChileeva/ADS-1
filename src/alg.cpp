733dfbdd22dcb421f30ab748e8ff7a24d2aeebcd
DariaChileeva <dariachileeva@yandex.ru>
2024-03-01 17:19:19 +0300

    ADS101

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,14 +3,14 @@
 
 
 bool checkPrime(uint64_t value) {
-    if(value<2){
+    if(value < 2){
         return false;
     }
-    if(value==2){
+    if(value == 2){
         return true;
     }
-    for(uint64_t i=2; i<value;i++){
-        if (value%i==0){
+    for(uint64_t i = 2; i < value;i++){
+        if (value % i==0){
             return false;
         }
     }
@@ -18,12 +18,12 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-uint64_t iPer=0;
-for(uint64_t i=2; i<UINTMAX_MAX;i++){
+uint64_t iPer = 0;
+for(uint64_t i = 2; i < UINTMAX_MAX;i++){
     if(checkPrime(i)){
         iPer++;
     }
-    if(n==iPer){
+    if(n == iPer){
         return i;
     }
 }
@@ -31,7 +31,7 @@ return 0;
 }
 
 uint64_t nextPrime(uint64_t value) {
-    for(uint64_t i=value+1; i<UINTMAX_MAX;i++){
+    for(uint64_t i = value+1; i < UINTMAX_MAX;i++){
         if(checkPrime(i)){
             return i;
         }
@@ -40,11 +40,11 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t sum=0;
-    for(uint64_t i=2; i<hbound; i++){
+    uint64_t summa = 0;
+    for(uint64_t i = 2; i < hbound; i++){
         if(checkPrime(i)){
-            sum=sum+i;
+            summa = summa + i;
         }
     }
-    return sum;
+    return summa;
 }
